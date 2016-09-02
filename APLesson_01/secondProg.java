4b22d3541cbe8a5dc9f2901b7e17b206138d08e0
Aaron Chen <ChenA6542@sduhsd.lan>
2016-09-02 13:07:44 -0700

    Added files

---
+++ APLesson_01/secondProg.java
@@ -0,0 +1,7 @@
+public class secondProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("(>^o^)>-[===>");
+	}
+}
---
+++ APLesson_01/thirdProg.java
@@ -0,0 +1,9 @@
+public class thirdProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  (\\_/)");
+		System.out.println(" (^o^)");
+		System.out.println("((  O  ))");
+	}
+}
